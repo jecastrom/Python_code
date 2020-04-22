88e49f32d7a02a8cd953842d3088feb5c18653b1
Jorge Castro <63274055+jcastrode@users.noreply.github.com>
2020-04-22 19:52:30 +0200

    No update

--- tip.py
+++ tip.py
@@ -1,26 +1,25 @@
-#Jorge Castro
-#Assignment: TIP calculator
-#Onemomth.com
+# Jorge Castro
+# Assignment: TIP calculator / 22.04.2020
+# Onemomth.com
 
 
-#Assigment of variable to store the user input
+# Assigment of variable to store the user input
+
+bill_amount = float(
+    input("Please enter your restaurant bill amount: ").strip("$"))
 
 
-
-bill_amount = float(input("Please enter your restaurant bill amount: ").strip("$"))
-
-
-#Variables to store the result of the operations and calculate tips
+# Variables to store the result of the operations and calculate tips
 total_tip15 = bill_amount * 0.15
 total_tip18 = bill_amount * 0.18
 total_tip20 = bill_amount * 0.20
 
-#Variable to store the total to pay
+# Variable to store the total to pay
 total_to_pay_15 = bill_amount + total_tip15
 total_to_pay_18 = bill_amount + total_tip18
 total_to_pay_20 = bill_amount + total_tip20
 
-#To make the result more readeble, multiple line on strings using """ """
+# To make the result more readeble, multiple line on strings using """ """
 
 print(f"""
 
@@ -33,7 +32,3 @@ print(f"""
  The Tip at 20% would be ${total_tip20:.2f} and the total to pay would be ${total_to_pay_20:.2f}
 
  """)
-
-
-
-
